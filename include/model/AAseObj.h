4a34e92fc17aa081836505d28cf06e3c2ca86a85
kimkyoungbok <rudqhr22@naver.com>
2018-12-19 18:49:14 +0900

    캐릭터 리소스 헤더 업로드

---
+++ include/model/AAseObj.h
@@ -0,0 +1,82 @@
+#pragma once
+#include "AAseParser.h"
+#include "AMesh.h"
+
+
+class AAseObj : public AModel
+{
+private:
+	const TCHAR*					m_FileName;
+public:
+	float					m_fTickSpeed;
+	float					m_fElapseTime;
+	float					fFrameTick=5;
+	int						m_iMaxVertex = 0;
+	int						m_iMaxIndex = 0;
+
+
+public:
+	AScene				m_Scene;
+	AAseParser			m_Parser;
+
+	vector<AMtrl>		m_MtrlList;			//텍스처 정보가 들어 있다.
+	vector<AGeomMesh*>	m_GeomObjList;		//지오브젝트가 들어 갈 구조체가 필요
+	
+											
+	vector<AMesh*>		m_MeshList;		//모든 정보를 여기에 담게 된다.
+
+public:
+	bool	Load(ID3D11Device* pd3dDevice,ID3D11DeviceContext* pContext ,const TCHAR* szLoadName, const TCHAR* pLoadShaderFile, bool bThread = false);
+	bool	Convert(ID3D11Device* pd3dDevice);
+
+	bool	CreateVertexBuffer(ID3D11Device* pd3dDevice);
+	bool	CreateIndexBuffer(ID3D11Device* pd3dDevice);
+	//bool	CreateConstantBuffer();
+
+
+	bool	LoadTexture(ID3D11Device* device, const TCHAR* fileName);
+
+	bool	Frame(const float& spf, const float& accTime)	noexcept override;
+	bool	Render(ID3D11DeviceContext* context)noexcept override;
+	
+public:	
+
+	bool	ConvertAnim(AGeomMesh* GObj, AMesh* pMesh);
+	void	AnimPlay(AMesh* pMesh, D3DXMATRIX* matParents, float fFrameTick);
+	bool	GetAnimationTrack(float fFrameTick,vector<AAnimTrack*> pTrackList, AAnimTrack** ppStartTrack, AAnimTrack** ppEndTrack);
+	AAnimTrack* SetLinkedTrack(AAnimTrack* pCurrentTrack, AAnimTrack* pPrev);
+
+
+	float PlayTrackTick(float fFrameTick, float fStartTick,float fEndTick);
+	AMesh* SearchToCollects(T_STR	m_strParentName);
+
+
+	bool AseRender(ID3D11DeviceContext*    pContext, AModel* pParent);
+	int GetMapID(AMtrl* pMtrl, int iTexMapType);
+
+
+	void	SetTriangleBuffer(AGeomMesh* GObjList, AMesh* pMesh);
+
+public:
+	AAseObj();
+	virtual ~AAseObj();
+};
+
+
+
+
+
+
+class AAnimation
+{
+public:
+	D3DXMATRIX	Interpolate(AMesh* pMesh, D3DXMATRIX* matParents, float fFrameTick, AScene tScene);
+	D3DXMATRIX	Update(float fElapseTime, AMesh* pMesh, D3DXMATRIX &matWorld, AScene &tScene);
+	bool		GetAnimationTrack(float fFrame, vector<AAnimTrack*>& pTrackList, AAnimTrack** pStartTrack, AAnimTrack** pEndTrack);
+public:
+	AAnimation(void);
+	virtual ~AAnimation(void);
+};
+
+
+
---
+++ include/model/_ActionList.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#define MatrixPATH L"../../data/shader/MatrixViewer.hlsl"
+#define TablePATH  L"../../data/Model/CharacterTable.cit"
+
+//참고 0 : 캐릭터 이름
+//*CHARACTERNAME		guard
+//*CHARACTERNAME		zombie
+
+//참고 1 : 액션리스트
+//////////////////////////////////////////////////////////////////////////
+//Guard
+//ANIM_COUNT  8
+
+#define	 Guard_LEFT			L"HERO_LEFT.Mat"
+#define	 Guard_RIGHT		L"HERO_RIGHT.Mat"
+#define	 Guard_BACKWARD		L"HERO_MOVE_BACK.Mat"
+#define	 Guard_FORWARD		L"HERO_MOVE.Mat"
+#define	 Guard_IDLE			L"HERO_IDLE.Mat"
+
+#define	 Guard_DANCE1		L"HERO_DANCE.mat"
+#define	 Guard_DANCE2		L"HERO_DANCE2.mat"
+#define	 Guard_DANCE3		L"HERO_DANCE3.mat"
+
+
+//////////////////////////////////////////////////////////////////////////
+//Zombie
+//ANIM_COUNT  5
+#define	 Zombie_LEFT		L"zombie_left.Mat"
+#define	 Zombie_RIGHT		L"zombie_right.Mat"
+#define	 Zombie_BACKWARD	L"zombie_back.Mat"
+#define	 Zombie_FORWARD		L"zombie_walk.Mat"
+#define	 Zombie_IDLE		L"zombie_idle.Mat"
+
+//참고 2 : 추가 방법 및 예제
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//#include AHero.h
+
+
+//////////////////////////////////Init()
+
+//I_CHARMGR.Init();
+//if (!I_CHARMGR.Load(DxManager::GetDevice(), DxManager::GetDContext(), _T("../../data/Model/CharacterTable.cit")))		//경로 중요함
+//{
+//	return false;
+//}
+
+//ex) AHero = m_Hero
+//m_Hero.SetPlayerCharacter(L"guard", 0, 0, 0);			//캐릭터  초기설정 설정 ---->> // 이름 중요함 -> 매니저를 통해서 불러움
+
+
+//애님 명령어
+//m_Hero.SetANIM(Guard_DANCE3);						//재생애니메이션 설정
+
+
+
+
+//////////////////////////////////Frame()
+//m_Hero.Frame(Timer::SPF, Timer::AccumulateTime);
+
+
+
+//////////////////////////////////Render()
+//m_Hero.SetMatrix(0, &ObjectManager::Get().Cameras[ECamera::Main]->m_matView, &ObjectManager::Get().Cameras[ECamera::Main]->m_matProj);	(월드,뷰,투영)
+//m_Hero.Render(DxManager::GetDContext());																									(랜더)
+
+
+//////////////////////////////////////////////////
