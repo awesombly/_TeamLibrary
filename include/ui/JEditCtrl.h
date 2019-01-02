d0c6e4b3476ee7d3603157946dbc7beea33e8013
AshiG <ashi0@naver.com>
2019-01-02 21:10:59 +0900

    ui 108번뇌

--- include/ui/JEditCtrl.h
+++ include/ui/JEditCtrl.h
@@ -1,23 +1,25 @@
 #pragma once
-#include "JTextCtrl.h"
+#include "JImageCtrl.h"
 namespace UI
 {
-	class JEditCtrl : public JTextCtrl
+	class JEditCtrl : public JImageCtrl
 	{
+	private:
+		//
+		wstring m_Text;
+		D3DXVECTOR4 m_rtText;
+		float m_fSize;
 	public:
-		bool m_bStart;
-		DWORD m_KeyBind;
-	public:
-		bool Create(ID3D11Device* pDevice, wstring Text, float TextSize,
-			const TCHAR* szNORMAL, const TCHAR* szHOVER, const TCHAR* szPRESS,
-			const char* PSName = "PS", const TCHAR* szShaderName = L"../../data/ui/shader/DefaultUI.hlsl");
+		bool Create(ID3D11Device* pDevice, float TextSize, const TCHAR* szNORMAL, 
+			 const char* PSName = "PS", const TCHAR* szShaderName = L"../../data/ui/shader/DefaultUI.hlsl");
+		bool SetString(wstring str);
 	public:
 		void Update();
 		bool Frame(const float& spf, const float& accTime) noexcept override;
 		bool Render(ID3D11DeviceContext* pContext)noexcept override;
 		bool Release()noexcept override;
 	public:
-		JEditCtrl(wstring NodeName) : JTextCtrl(NodeName) 
+		JEditCtrl(wstring NodeName) : JImageCtrl(NodeName)
 		{
 			m_Type = UIType::EDIT;
 		};
