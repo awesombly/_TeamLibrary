396e103d3be559b88c2f63a013964432cf1d70bb
kimkyoungbok <rudqhr22@naver.com>
2019-01-30 10:21:00 +0900

    사운드::고고 주석

--- src/test 3D/LobbyScene.cpp
+++ src/test 3D/LobbyScene.cpp
@@ -226,7 +226,7 @@ void LobbyScene::StartToHost()
 	}	break;
 	}
 	m_pStartEffect->EffectPlay();
-	SoundManager::Get().Play("SV_gogogo.mp3");
+	//SoundManager::Get().Play("SV_gogogo.mp3");
 	PacketManager::Get().isHost = true;
 	m_isStart = true;
 }
