ac70e3a84fd2570d492fb37fee084c56460f55fb
kimkyoungbok <rudqhr22@naver.com>
2019-01-30 17:44:42 +0900

    대시 소리 주석

--- src/test 3D/PlayerState.cpp
+++ src/test 3D/PlayerState.cpp
@@ -384,7 +384,7 @@ bool ArcherStateBasic::Process(const float& spf) noexcept
 
 		m_pOwner->m_DelayFrame = 1.0f;
 		m_pOwner->m_curDelayDash = m_pOwner->m_DelayDash;
-		SoundManager::Get().Play("SE_jump02.mp3");
+		//SoundManager::Get().Play("SE_jump02.mp3");
 	}
 
 	// È­»ìºñ
@@ -633,7 +633,7 @@ bool MageStateBasic::Process(const float& spf) noexcept
 			m_pOwner->m_DelayFrame = m_pOwner->m_DelayDash * 0.4f;
 			m_pOwner->m_curDelayDash = m_pOwner->m_DelayDash;
 			m_pOwner->m_eAction = PlayerController::EAction::Dash;
-			SoundManager::Get().Play("SE_jump02.mp3");
+			//SoundManager::Get().Play("SE_jump02.mp3");
 		}
 	}
 
